90f139061a23436aa72b7bb1ae96fb8184433f46
liaopen123 <467969@qq.com>
2020-11-28 13:36:35 +0800

    test variable

---
+++ index.js
@@ -0,0 +1,16 @@
+var request = require('request');
+
+const url = 'http://custom.luyuan.cn/custom/signin/add';
+
+const userCode = process.env.USERCODE
+console.log("得到的userCode:" + userCode)
+// request.post({
+//     url: url,
+//     form: {
+//         usercode: '北京'
+//     }
+// }, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//         console.log(body);// 请求成功的处理逻辑
+//     }
+// });
