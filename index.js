ca3296a9631ec9144ec5aac815301e44636648c1
liaopen123 <467969@qq.com>
2020-11-28 14:02:55 +0800

    add cookies

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var request = require('request');
 
 const url = 'http://custom.luyuan.cn/custom/signin/add';
 
-const userCode = process.env.USERCODE
+var userCode = process.env.USERCODE
 console.log("得到的userCode:${userCode}" + userCode)
-// request.post({
-//     url: url,
-//     form: {
-//         usercode: '北京'
-//     }
-// }, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//         console.log(body);// 请求成功的处理逻辑
-//     }
-// });
+request.post({
+    url: url,
+    headers: {
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36',
+    },
+    form: {
+        usercode: userCode
+    }
+}, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);// 请求成功的处理逻辑
+    }
+}).set('Cookie', 'UM_distinctid=175bb9b178e929-0cf52312fb0ef5-326e7207-60e640-175bb9b178f757');
